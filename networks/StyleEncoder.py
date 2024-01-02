070999828907368f9d3462f90b32be475dc30da4
Christoph Gerhardt <christoph.gerhardt@tu-ilmenau.de>
2024-01-02 16:32:51 +0100

    Move ResBlk to blocks.py

--- networks/StyleEncoder.py
+++ networks/StyleEncoder.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-from . import blocks
+from blocks import ResBlk
 
 class StyleEncoder(nn.Module):
     def __init__(self, input_channels=3, generator_in_filters=64, style_dim=8, n_downsampling=2, no_antialias=False, num_domains=2):
@@ -126,46 +126,3 @@ class MappingNetwork(nn.Module):
         idx = torch.LongTensor(range(y.size(0))).to(y.device)
         s = out[idx, y]  # (batch, style_dim)
         return s    
-        
-class ResBlk(nn.Module):
-    def __init__(self, dim_in, dim_out, actv=nn.LeakyReLU(0.2),
-                 normalize=False, downsample=False):
-        super().__init__()
-        self.actv = actv
-        self.normalize = normalize
-        self.downsample = downsample
-        self.learned_sc = dim_in != dim_out
-        self._build_weights(dim_in, dim_out)
-
-    def _build_weights(self, dim_in, dim_out):
-        self.conv1 = nn.Conv2d(dim_in, dim_in, 3, 1, 1)
-        self.conv2 = nn.Conv2d(dim_in, dim_out, 3, 1, 1)
-        if self.normalize:
-            self.norm1 = nn.InstanceNorm2d(dim_in, affine=True)
-            self.norm2 = nn.InstanceNorm2d(dim_in, affine=True)
-        if self.learned_sc:
-            self.conv1x1 = nn.Conv2d(dim_in, dim_out, 1, 1, 0, bias=False)
-
-    def _shortcut(self, x):
-        if self.learned_sc:
-            x = self.conv1x1(x)
-        if self.downsample:
-            x = F.avg_pool2d(x, 2)
-        return x
-
-    def _residual(self, x):
-        if self.normalize:
-            x = self.norm1(x)
-        x = self.actv(x)
-        x = self.conv1(x)
-        if self.downsample:
-            x = F.avg_pool2d(x, 2)
-        if self.normalize:
-            x = self.norm2(x)
-        x = self.actv(x)
-        x = self.conv2(x)
-        return x
-
-    def forward(self, x):
-        x = self._shortcut(x) + self._residual(x)
-        return x / math.sqrt(2)  # unit variance    
