bf9394b12551da457b9aa34e7c0255d0f5da6e41
Christoph Gerhardt <christoph.gerhardt@tu-ilmenau.de>
2024-01-10 00:51:57 +0100

    Supress warning while color encoding

--- helper/helper_utils.py
+++ helper/helper_utils.py
@@ -16,14 +16,18 @@ def colorEncode(labelmap, colors, mode='RGB'):
     labelmap = labelmap.astype('uint8')
     labelmap_rgb = np.zeros((labelmap.shape[0], labelmap.shape[1], 3),
                             dtype=np.uint8)
-    if labelmap.max() > len(colors):
+    labels = np.unique(labelmap)
+    if len(labels) > len(colors) + 1:
         print('Error: labelmap contains too many labels - {}'.format(labelmap.max()))
-    for label in np.unique(labelmap):
+    for label in labels:
         if label < 0:
             continue
         if label == 255:
-            label = -1
-        labelmap_rgb += (labelmap == label)[:, :, np.newaxis] * \
+            labelmap_rgb += (labelmap == label)[:, :, np.newaxis] * \
+            np.tile((0, 0, 0),
+                    (labelmap.shape[0], labelmap.shape[1], 1)).astype(np.uint8)        
+        else:
+            labelmap_rgb += (labelmap == label)[:, :, np.newaxis] * \
             np.tile(colors[label],
                     (labelmap.shape[0], labelmap.shape[1], 1)).astype(np.uint8)
 
