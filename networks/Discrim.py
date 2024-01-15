5c0846a8fa045ed885445dc1bff1507073018fde
Christoph Gerhardt <christoph.gerhardt@tu-ilmenau.de>
2024-01-15 17:42:27 +0100

    Remove StarGAN discriminator

--- networks/Discrim.py
+++ networks/Discrim.py
@@ -1,6 +1,7 @@
 import functools 
 import torch
 import torch.nn as nn
+import logging
 
 from . import blocks
 
@@ -64,31 +65,4 @@ class NLayerDiscriminator(nn.Module):
         idx = torch.LongTensor(range(y.size(0))).to(y.device)
         y = y.type(torch.int32)
         out = out[:, y, :, :]  # (batch)
-        return out    
-    
-    #StarGan Discriminator
-    class Discriminator(nn.Module):
-        def __init__(self, img_size=256, num_domains=2, max_conv_dim=512):
-            super().__init__()
-            dim_in = 2**14 // img_size
-            blocks = []
-            blocks += [nn.Conv2d(3, dim_in, 3, 1, 1)]
-
-            repeat_num = int(np.log2(img_size)) - 2
-            for _ in range(repeat_num):
-                dim_out = min(dim_in*2, max_conv_dim)
-                blocks += [ResBlk(dim_in, dim_out, downsample=True)]
-                dim_in = dim_out
-
-            blocks += [nn.LeakyReLU(0.2)]
-            blocks += [nn.Conv2d(dim_out, dim_out, 4, 1, 0)]
-            blocks += [nn.LeakyReLU(0.2)]
-            blocks += [nn.Conv2d(dim_out, num_domains, 1, 1, 0)]
-            self.main = nn.Sequential(*blocks)
-
-        def forward(self, x, y):
-            out = self.main(x)
-            out = out.view(out.size(0), -1)  # (batch, num_domains)
-            idx = torch.LongTensor(range(y.size(0))).to(y.device)
-            out = out[idx, y]  # (batch)
-            return out
+        return out
