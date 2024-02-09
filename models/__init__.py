250c676f539f714a325925168210ac07b6cb48a7
carhartt21 <chrgt20@t-online.de>
2024-02-09 16:36:51 +0100

    Remove missing import

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 from .content_encoder import ContentEncoder, ContentEncoderV2
-from .style_encoder import StyleEncoder, MLP, MappingNetwork
+from .style_encoder import StyleEncoder, MappingNetwork
 from .transformer import Transformer
 from .generator import Generator
 from .discriminator import NLayerDiscriminator
